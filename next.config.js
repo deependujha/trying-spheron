fa8b2d0daa95119eb470550b6100c43d5f7577a2
deependujha <deependujha21@gmail.com>
2022-12-01 02:01:06 +0530

    modified next.config.js file as some functions were not working

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-};
 
 module.exports = {
-	nextConfig,
+	reactStrictMode: true,
+
 	images: {
 		loader: 'akamai',
 		path: '',
